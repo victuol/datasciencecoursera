3aff4a245ddef317c9eb7f0b34efcefa7a40e7b9
victuol <victuol@yahoo.com>
2014-06-21 12:15:13 -0500

    Create ProgrammingAssignment2.R

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,38 @@
+datasciencecoursera
+===================
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+
+m<-NULL
+  set<-function(y){
+  x<<-y
+  m<<-NULL
+}
+get<-function() x
+setmatrix<-function(solve) m<<- solve
+getmatrix<-function() m
+list(set=set, get=get,
+   setmatrix=setmatrix,
+   getmatrix=getmatrix)
+
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+
+
+m<-x$getmatrix()
+    if(!is.null(m)){
+      message("obtaining cached data")
+      return(m)
+    }
+    matrix<-x$get()
+    m
+        ## Return a matrix that is the inverse of 'x'
+}
